6782f6fbe848a591a2d5dd7ec35166b13d673c1e
Vipul Sharma <116291385+vipulsharma17@users.noreply.github.com>
2024-11-04 15:24:14 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -108,7 +108,9 @@ portfolio = {
         "https://www.youtube.com/watch?v=xmSnpyS5oj0",
         "https://www.youtube.com/watch?v=u_GQoqSXmpw",
         "https://www.youtube.com/watch?v=4R3j2SLbuUo"
-    ]
+    ],
+    "Short Form Content ": [
+        "https://www.youtube.com/@ThePodiumCast/shorts"
 }
 
 st.title("Video Editor Portfolio")
